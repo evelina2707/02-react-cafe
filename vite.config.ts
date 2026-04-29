d619f4bc71972ff4b4bb1cd7a9f641fcf2bdeb4a
Evelina <evelina20082707@gmail.com>
2026-04-29 23:24:41 +0300

    fix github pages base

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,14 @@ import babel from "@rolldown/plugin-babel";
 
 // https://vite.dev/config/
 export default defineConfig({
- plugins: [
-react(),
-babel({
-presets: [reactCompilerPreset()],
-}),
-],
-build: {
- sourcemap: true,
-},
+  base: "/02-react-cafe/",
+  plugins: [
+    react(),
+    babel({
+      presets: [reactCompilerPreset()],
+    }),
+  ],
+  build: {
+    sourcemap: true,
+  },
 });
